bb001776c951577b709395d455cfecdd2ac5b15b
Tim Siwula <{ID}+{username}@users.noreply.github.com>
2023-11-18 00:08:11 -0600

    :bowtie: [auto generated] via tim's deploy_script.sh

--- building-react-and-django-apps/03_06_end/frontend/src/AppContext.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const AppContext = React.createContext({
-  booking: null,
-  wishlist: [],
-  toggleWishlist: () => {},
-  addToCart: () => {},
-  removeFromCart: () => {}
-});
